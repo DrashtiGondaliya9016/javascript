a5fd6479fa6671eacc048573d4a9c86f3ad619f6
Drashti <drashtidevani47@gmail.com>
2025-09-19 10:11:36 +0530

    variables

---
+++ column preview card/3-column-preview-card-component-main/script.js
@@ -0,0 +1,9 @@
+
+
+var  card1 = document.querySelectorAll('.card1');
+ card1[0].addEventListener('click', function () {
+    //    alert('You clicked the paragraph!');
+    this.style.backgroundColor = 'red';
+});
+
+console.log('card card1',  card1);
---
+++ column preview card/variables/script.js
@@ -0,0 +1,13 @@
+        var ele = document.getElementById('b');
+        console.log(ele);
+
+        var ele = document.getElementsByClassName('a');
+        console.log(ele);
+
+        var ele = document.getElementsByTagName("div");
+        console.log(ele);
+
+        var ele = document.images;
+        console.log(ele);
+
+     
